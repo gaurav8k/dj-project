86c2ae32b374ac97119921ec3eb919be19d6db7d
gaurav8k <gaurav8k@gmail.com>
2017-01-21 23:54:25 +0530

    Database added
    database connection and added few pages for albums

--- music/views.py
+++ music/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>This is music app homepage..</h1>")
+    all_albums = Album.objects.all()
+    context = {
+        'all_albums': all_albums,
+    }
+    return render(request, 'music/index.html', context) #bydefault django will look for this file inside templates directory
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album: " + str(album_id) + "</h2>")
